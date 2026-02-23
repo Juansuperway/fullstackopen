25185576a46506ae138f637f902e6bc6a5c1f7d0
Juan Sanchez Robles <es00522502@prosegur.com>
2026-02-23 09:44:45 +0100

    exercise 1.8 extract Statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,17 +10,28 @@ const Button = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const goodCount = good
-  const neutralCount = neutral
-  const badCount = bad
   const all = good + neutral + bad
-  const positive = (good) / all
-  const average = ((good - bad) / all) * 100
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
   return (
     <div>
       <h1>Give feedback</h1>
@@ -29,13 +40,8 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
       
-
-      <p>Good: {goodCount}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
+      
     </div>
   )
 }
