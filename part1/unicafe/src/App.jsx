a05b49299fd037b862ed65ceabdeda8c23cbffb1
Juan Sanchez Robles <es00522502@prosegur.com>
2026-02-23 10:46:47 +0100

    exercise 1.9 conditional rendering no feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,15 +11,25 @@ const Button = (props) => {
 }
 
 const Statistics = (props) => {
+  if (props.all == 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feddback given</p>
+      </div>
+    )
+    
+  }
   return (
     <div>
-      <h2>Statistics</h2>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p>
-      <p>Average: {props.average}</p>
-      <p>Positive: {props.positive} %</p>
+      <h2>Statistics</h2>      
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>All: {props.all}</p>
+        <p>Average: {props.average}</p>
+        <p>Positive: {props.positive} %</p>
+      
     </div>
   )
 }
