b07412f9898dffe981106243726838d377614fbf
Juan Sanchez Robles <es00522502@prosegur.com>
2026-02-27 11:35:37 +0100

    exercise 2.2 courseinfo show total

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -26,7 +26,9 @@ const Course = (props) => {
     <div>
       <Header course={props.course.name} />
       <Content parts={props.course.parts} />
-      
+      <Total
+        parts={props.course.parts}
+      />
     </div>
   )
 }
