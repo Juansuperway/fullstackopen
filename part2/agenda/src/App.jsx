d7dee1a7905238c7758fff45527570586ce381bd
Juan Sanchez Robles <es00522502@prosegur.com>
2026-03-02 17:06:08 +0100

    exercise 2.7 agenda step 2

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -15,8 +15,15 @@ const App = () => {
     const personObject = {
       name: newName
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+
+    
     
 
   }
