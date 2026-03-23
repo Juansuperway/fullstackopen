3a520a06dc7a8fdd032f4eb8886fde8ac651a58c
Juan Sanchez Robles <es00522502@prosegur.com>
2026-03-23 14:05:12 +0100

    exercise 3.3 agenda filter person

--- part3/index.js
+++ part3/index.js
@@ -45,7 +45,12 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    response.json(person)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 
